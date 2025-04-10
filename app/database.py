58f22f57b2a32652e7ae88673a92d1f3cce0e480
Samkelo Siyabonga Ngubo <samke704@gmail.com>
2025-04-10 20:03:28 +0200

    update work

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 # Access variables
 
-DATABASE_URL = os.getenv("postgresql://root:fMv672jJSi3buIwAp1BGKD8Ctv4iaI19@dpg-cvleqd49c44c73fduukg-a/restorelaondb")
+DATABASE_URL = os.getenv("DATABASE_URL")
 SQLALCHEMY_DATABASE_URL = DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
