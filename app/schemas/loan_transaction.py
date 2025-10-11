23971afc7dd0d4b6a8fb619caab5c02a4d5cb2a6
Samkelo Siyabonga Ngubo <samke704@gmail.com>
2025-10-11 15:47:31 +0200

    updated code

--- app/schemas/loan_transaction.py
+++ app/schemas/loan_transaction.py
@@ -1,27 +1,23 @@
-from pydantic import BaseModel
 from typing import Optional
+from pydantic import BaseModel
 from datetime import datetime
-
 
 class LoanTransactionBase(BaseModel):
     loan_id: int
     user_id: int
     borrower: str
     loan_amount: float
-    account_no: str  # <-- Added field
-
+    account_no: Optional[str] = None
 
 class LoanTransactionCreate(LoanTransactionBase):
     pass
 
-
 class LoanTransactionUpdateStatus(BaseModel):
-    status_approval: str  # "Pending", "Approved", "Declined"
-
+    status_approval: str
 
 class LoanTransactionResponse(LoanTransactionBase):
     id: int
-    date_applied: datetime
+    date_applied: Optional[datetime] = None
     status_approval: str
     date_approved: Optional[datetime] = None
 
