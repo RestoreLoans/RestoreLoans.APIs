318a3165359a81ad0f75ede2176fd896b61c3dcb
Samkelo Siyabonga Ngubo <samke704@gmail.com>
2025-10-11 17:27:32 +0200

    updated

--- app/schemas/company.py
+++ app/schemas/company.py
@@ -2,14 +2,13 @@ from pydantic import BaseModel, EmailStr
 from typing import Optional
 from datetime import datetime
 
+
 class CompanyBase(BaseModel):
     name: str
     address: Optional[str] = None
     email: Optional[EmailStr] = None
     phone: Optional[str] = None
-    user_id: Optional[str] = None
-
-
+    user_id: Optional[int] = None
 
 
 class CompanyCreate(CompanyBase):
@@ -22,17 +21,12 @@ class CompanyCreate(CompanyBase):
 
 class CompanyResponse(CompanyBase):
     id: int
+    user_id: Optional[int] = None
     address: Optional[str] = None
     email: Optional[EmailStr] = None
     phone: Optional[str] = None
     created_at: datetime
     updated_at: datetime
-
-    class Config:
-        from_attributes = True
-   
-
-
 
     class Config:
         from_attributes = True
