11d3fa89ae4321cb8d092b02b131603e6bef20ba
Samkelo Siyabonga Ngubo <samke704@gmail.com>
2026-08-25 14:19:35 +0200

    Fix load_dotenv path to resolve app/.env correctly

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -5,7 +5,7 @@ from typing import List
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"))
 
 
 class EmailService:
