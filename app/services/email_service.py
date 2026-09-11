63532cd73db523034a5477647648f13b1d591a1a
Samkelo Siyabonga Ngubo <samke704@gmail.com>
2026-09-11 16:44:09 +0200

    Force .env to override process env so SMTP always uses configured mail server

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -9,7 +9,7 @@ import enum as _enum
 import os
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"))
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"), override=True)
 
 
 class EmailService:
